80247e7c679fef8cf87022851a58faac57855cdc
kunalkarpe <kunalkarpe32@gmail.com>
2025-01-20 00:08:44 +0530

    feeat : sidebar : added drag and drop in sidebar

--- apps/sidebar/src/component/Sidebar/Sidebar.tsx
+++ apps/sidebar/src/component/Sidebar/Sidebar.tsx
@@ -1,10 +1,14 @@
+/** @format */
+
 import { PanelLeftClose, PanelRightClose } from "lucide-react";
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { FytuneLogo, MiniFyntuneLogo } from "../../assets/index";
 import CurrentUserCard from "./component/CurrentUserCard";
 import SideTabs from "./component/SideTabs";
 import { CURRENT_USER_DATA } from "./sidebar.constant";
 import { SidebarContext } from "../../ContextProvider/SidebarContext/SidebarContextProvide";
+import { AnimatePresence } from "motion/react";
+import * as motion from "motion/react-client";
 const Sidebar = () => {
   const { closeSideBar, setCloseSidebar } = useContext(SidebarContext);
   return (
@@ -12,36 +16,36 @@ const Sidebar = () => {
       className={`flex flex-col gap-6 h-full p-2 bg-white rounded-xl  ${closeSideBar === true ? "w-16" : "w-full"}`}
     >
       <div className="flex flex-col gap-4 h-[23rem] border-b border-extraLightGray p-2">
-        <div className={` justify-between h-12 items-center ${closeSideBar===false? "flex":"flex flex-col gap-2"}`}>
+        <div
+          className={` justify-between h-v12 items-center ${closeSideBar === false ? "flex" : "flex flex-col gap-2"}`}
+        >
           {closeSideBar === false ? (
             <img src={FytuneLogo} alt="" className=" w-36 h-8" />
           ) : (
             <img src={MiniFyntuneLogo} alt="" className=" w-36 h-8" />
           )}
- 
-    <button
-    onClick={() => {
-      setCloseSidebar(!closeSideBar);
-    }}
-    type="button"
-  >
-    {closeSideBar === Boolean(true) ? (
-      <PanelRightClose className="size-4 text-body" />
-    ) : (
-      <PanelLeftClose className="size-4 text-body" />
-    )}
-  </button> 
-      
+
+          <button
+            onClick={() => {
+              setCloseSidebar(!closeSideBar);
+            }}
+            type="button"
+          >
+            {closeSideBar === Boolean(true) ? (
+              <PanelRightClose className="size-4 text-body" />
+            ) : (
+              <PanelLeftClose className="size-4 text-body" />
+            )}
+          </button>
         </div>
+
         <SideTabs closeSideBar={closeSideBar} />
       </div>
-
 
       <CurrentUserCard
         cardData={CURRENT_USER_DATA}
         closeSideBar={closeSideBar}
       />
-
     </div>
   );
 };
--- apps/sidebar/src/component/Sidebar/component/SideTabs.tsx
+++ apps/sidebar/src/component/Sidebar/component/SideTabs.tsx
@@ -1,24 +1,56 @@
+/** @format */
+
 import React, { useState } from "react";
 import { SIDEBAR_TABS } from "../sidebar.constant";
+import { filter } from "motion/react-client";
 const SideTabs = ({ closeSideBar }) => {
   const [isActive, setActive] = useState<number>(SIDEBAR_TABS[0]?.id);
+  const [showName, setShowName] = useState(false);
+  const [sideTab, setSideTabs] = useState(SIDEBAR_TABS);
+  const [sideTabId, setSideTabId] = useState(0);
+
+  const handelDragStart = (id) => {
+    setSideTabId(id);
+  };
+
+  const handeDragEnd = (index) => {
+    if (index === sideTabId) return;
+    const prevsTabs = [...sideTab];
+    [prevsTabs[sideTabId], prevsTabs[index]] = [
+      prevsTabs[index],
+      prevsTabs[sideTabId],
+    ];
+    console.log(prevsTabs);
+    setSideTabs(prevsTabs);
+  };
 
   return (
     <div className="flex flex-col gap-1">
-      {SIDEBAR_TABS?.map((sideTab) => {
+      {sideTab?.map((sideTab, index) => {
         const isActivated = sideTab?.id === isActive;
+
         return (
-          <button
-            type="button"
-            className={`flex  text-xs gap-4 h-9 items-center rounded-lg px-2 ${isActivated ? "bg-primary/90 text-white border border-extraLightGray" : ""}`}
+          <div
+            className="flex"
+            onDragStart={() => handelDragStart(index)}
+            onDragOver={(e) => e.preventDefault()}
+            onDrop={() => {
+              handeDragEnd(index);
+            }}
+            draggable={true}
             key={sideTab?.id}
-            onClick={() => setActive(sideTab?.id)}
-
           >
-            <p>{sideTab?.icon}</p>
-            {closeSideBar === false && <p>{sideTab?.name}</p>}
-
-          </button>
+            <button
+              type="button"
+              className={`flex w-full text-xs gap-4 h-9 items-center rounded-lg px-2 ${isActivated ? "bg-primary/90 text-white border border-extraLightGray" : ""}`}
+              onClick={() => setActive(sideTab?.id)}
+            >
+              <p>{sideTab?.icon}</p>
+              {(closeSideBar === false || isActivated) && (
+                <p>{sideTab?.name}</p>
+              )}
+            </button>
+          </div>
         );
       })}
     </div>
